2261f9fe3e0d133f60b9b55ad7ea642fe20a363d
Felipe Bueno <bueno.felipe@gmail.com>
2015-06-25 15:03:31 -0300

    first commit after moving location to this new repository

---
+++ app/src/main/java/peccin/location/FollowMeServiceKiller.java
@@ -0,0 +1,18 @@
+package peccin.location;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class FollowMeServiceKiller extends BroadcastReceiver {
+
+    public static FollowMeService victim;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        victim.stopForeground(true);
+        victim.stopSelf();
+    	FollowMeService.isRunning = false;
+    }
+
+}
